3ecd708dc7acdb63fd708187cbbb38a0533e79f7
tracer-ics2016 <tracer@njuics.org>
2016-09-15 08:41:30 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  08:41:30 up  1:45,  1 user,  load average: 0.00, 0.00, 0.00 33f1c98bed8a17d87a56f4de92516d55fd7d1ba

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -30,7 +30,7 @@ static struct rule {
 	{"\\/", '/'},
 	{"\\(", '('},
 	{"\\)", ')'},
-	{"[0-9]*[.]?[0-9]*", NUM}
+	{"[0-9]+[.]?[0-9]*|[0-9]*[.]?[0-9]+", NUM}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
