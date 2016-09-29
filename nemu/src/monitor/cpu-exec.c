f45398020ec5d1b1a1aecf29775a14d0e02929bd
tracer-ics2016 <tracer@njuics.org>
2016-09-29 15:56:05 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:56:05 up 20:31,  1 user,  load average: 0.00, 0.01, 0.00 a364938dcf78f13e1bc0955fa7ea684c1dfcf812

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -77,13 +77,10 @@ void cpu_exec(volatile uint32_t n) {
 
 		int stop = check_wp();
 		if (stop != 0) {
-			print_bin_instr(eip_temp, instr_len);
-			printf("%s\n", asm_buf);
-			strcat(asm_buf, assembly);
 			printf("%s\n", asm_buf);
 			do_int3();
 			print_bin_instr(cpu.eip, instr_len);
-			printf("%s\n", asm_buf);
+			Log_write("%s\n", asm_buf);
 			strcat(asm_buf, assembly);
 			printf("%s\n\n", asm_buf);
 		}
