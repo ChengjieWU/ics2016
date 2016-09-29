dfbbb698d9eb5168509e61d0bb5f8e2b6ad83cce
tracer-ics2016 <tracer@njuics.org>
2016-09-29 16:42:09 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  16:42:09 up 21:17,  1 user,  load average: 0.00, 0.00, 0.00 3d92f37e2958bbbf57892618de59a5462dd520d

--- nemu/src/cpu/exec/ctf/call.c
+++ nemu/src/cpu/exec/ctf/call.c
@@ -4,7 +4,7 @@ make_helper(call_rel32) {
 	reg_l(R_ESP) = reg_l(R_ESP) - 4;
 	swaddr_write(reg_l(R_ESP), 4, cpu.eip + 5);
 	swaddr_t reldisp = instr_fetch(cpu.eip + 1, 4);
-	reldisp += 5;
-	print_asm("call %x", cpu.eip + reldisp);
+	print_asm("call %x", cpu.eip + 5 + reldisp);
+	cpu.eip += reldisp;
 	return 5; 
 }
