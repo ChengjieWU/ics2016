b2017dd7c21865c40ca64c60c8d4af5d87488d0c
tracer-ics2016 <tracer@njuics.org>
2016-09-30 11:13:54 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  11:13:54 up 23:09,  1 user,  load average: 0.00, 0.00, 0.00 e96f8487dc594aabd34739e7762e56f3428cfd1e

--- nemu/src/cpu/exec/ctf/call-template.h
+++ nemu/src/cpu/exec/ctf/call-template.h
@@ -3,11 +3,19 @@
 #define instr call
 
 make_helper(concat(call_rel_, SUFFIX)) {
-	REG(R_ESP) = REG(R_ESP) - 4;
-	MEM_W(REG(R_ESP), eip + 1 + DATA_BYTE);
-	swaddr_t reldisp = instr_fetch(eip + 1, DATA_BYTE);
-	print_asm("call %x", eip + 1 + DATA_BYTE + reldisp);
-	cpu.eip = eip + reldisp;
+	REG(R_ESP) = REG(R_ESP) - DATA_BYTE;	
+	if (DATA_BYTE == 4) {
+		MEM_W(REG(R_ESP), eip + 1 + DATA_BYTE);
+		swaddr_t reldisp = instr_fetch(eip + 1, DATA_BYTE);
+		print_asm("call %x", eip + reldisp + 1 + DATA_BYTE);
+		cpu.eip = eip + reldisp;
+	}
+	else if (DATA_BYTE == 2) {
+		MEM_W(REG(R_ESP), (eip + 1 + DATA_BYTE) & 0x0000ffff);
+		swaddr_t reldisp = instr_fetch(eip + 1, DATA_BYTE);
+		print_asm("call %x", (eip + reldisp + 1 + DATA_BYTE) & 0x0000ffff);
+		cpu.eip = (eip + reldisp) & 0x0000ffff;
+	}	//eip cannot write to cpu.eip
 	return 1 + DATA_BYTE;
 }
 
