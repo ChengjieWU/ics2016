c43db93d2255f9499117d3ec3e7c6eec0f3cf994
tracer-ics2016 <tracer@njuics.org>
2016-10-02 15:34:55 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:34:55 up 1 day, 10:29,  1 user,  load average: 0.01, 0.03, 0.00 165591ae98f1a8c7ad719ff1d207bf89970c60fd

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -6,6 +6,7 @@ make_helper(concat(pop_r_, SUFFIX)) {
 	int regnum = instr_fetch(eip, 1) & 0x7;
 	REG(regnum) = MEM_R(REG(R_ESP));
 	REG(R_ESP) -= DATA_BYTE;
+	print_asm("pop %%%s", REG_NAME(regnum));
 	return 1;
 }
 
@@ -13,6 +14,7 @@ make_helper(concat(pop_m_, SUFFIX)) {
 	int len = concat(decode_rm_, SUFFIX)(eip + 1);
 	MEM_W(op_src->addr, MEM_R(REG(R_ESP)));
 	REG(R_ESP) -= DATA_BYTE;
+	print_asm("pop 0x%x", op_src->addr);
 	return 1 + len;
 }
 
