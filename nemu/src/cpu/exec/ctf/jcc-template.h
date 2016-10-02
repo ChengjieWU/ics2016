9485dcfefe79342806f7935fdb811bf6b806fc50
tracer-ics2016 <tracer@njuics.org>
2016-10-02 08:15:08 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  08:15:08 up 1 day,  5:44,  1 user,  load average: 0.00, 0.00, 0.00 f5dcb365310787bf70782539b14bf1e7834fdfc8

--- nemu/src/cpu/exec/ctf/jcc-template.h
+++ nemu/src/cpu/exec/ctf/jcc-template.h
@@ -4,14 +4,8 @@
 
 make_helper(concat(jcc_e_,SUFFIX)) {
 	swaddr_t jccdisp = instr_fetch(eip + 1, DATA_BYTE);
-	if (DATA_BYTE == 4) {
-		print_asm("je 0x%x", cpu.eip + jccdisp + 1 + DATA_BYTE);
-		if (cpu.ZF == 1) cpu.eip += jccdisp;
-	}
-	else if (DATA_BYTE == 2) {
-		print_asm("je 0x%x", (cpu.eip + jccdisp + 1 + DATA_BYTE) & 0x0000ffff);
-		if (cpu.ZF == 1) cpu.eip = (cpu.eip + jccdisp) & 0x0000ffff;
-	}
+	print_asm("je 0x%x", cpu.eip + jccdisp + 1 + DATA_BYTE);
+	if (cpu.ZF == 1) cpu.eip += jccdisp;
 	return 1 + DATA_BYTE;	
 }
 
--- nemu/src/cpu/exec/ctf/jcc.h
+++ nemu/src/cpu/exec/ctf/jcc.h
@@ -1,6 +1,7 @@
 #ifndef __JCC_H__
 #define __JCC_H__
 
+make_helper(jcc_e_b);
 make_helper(jcc_e_v);
 
 #endif
