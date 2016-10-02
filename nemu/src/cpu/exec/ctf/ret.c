b1cf7977c1f84779ae932bb0d819a23a4f021bd0
tracer-ics2016 <tracer@njuics.org>
2016-10-02 16:59:56 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  16:59:56 up 1 day, 11:54,  1 user,  load average: 0.09, 0.03, 0.01 56c247e4032f0544f3fb8ee98ff685a96c91fe2

--- nemu/src/cpu/exec/ctf/ret.c
+++ nemu/src/cpu/exec/ctf/ret.c
@@ -2,9 +2,13 @@
 
 make_helper(ret_near) {
 	cpu.esp = cpu.ebp + 4;
+	printf("0x%x\n", cpu.esp);
 	cpu.ebp = swaddr_read(cpu.ebp, 4);
+	printf("0x%x\n", cpu.ebp);
 	cpu.eip = swaddr_read(cpu.esp, 4) - 1;
+	printf("0x%x\n", cpu.eip);
 	cpu.esp += 4;
+	printf("0x%x\n", cpu.esp);
 	print_asm("ret");
 	return 1;
 }
