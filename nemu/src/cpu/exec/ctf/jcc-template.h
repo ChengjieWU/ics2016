0780174e297183ee3c999d28dd48f799c4470d72
tracer-ics2016 <tracer@njuics.org>
2016-10-13 01:11:32 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  01:11:32 up  1:15,  1 user,  load average: 0.03, 0.01, 0.00 7b187b3b9ac0853fa5e93274181c3e95cfee02fe

--- nemu/src/cpu/exec/ctf/jcc-template.h
+++ nemu/src/cpu/exec/ctf/jcc-template.h
@@ -3,17 +3,20 @@
 #define instr jcc
 
 make_helper(concat(jcc_e_,SUFFIX)) {
-	swaddr_t jccdisp = instr_fetch(eip + 1, DATA_BYTE);
+	DATA_TYPE jccdisp = instr_fetch(eip + 1, DATA_BYTE);
 	print_asm("je %x", cpu.eip + jccdisp + 1 + DATA_BYTE);
 	if (cpu.ZF == 1) cpu.eip += jccdisp;
 	return 1 + DATA_BYTE;	
 }
 
 make_helper(concat(jcc_be_,SUFFIX)) {
-	swaddr_t jccdisp = instr_fetch(eip + 1, DATA_BYTE);
-	print_asm("jbe %x", cpu.eip + 1 + DATA_BYTE + jccdisp);
-	if (cpu.ZF == 1 || cpu.CF == 1) cpu.eip += jccdisp;
-	return 1 + DATA_BYTE;
+	eip += 1 + DATA_BYTE;
+	DATA_TYPE cur_lo = (DATA_TYPE)eip + instr_fetch(eip + 1, DATA_BYTE);
+	DATA_TYPE ze = ~0x0;
+	swaddr_t cur_addr = (eip & (~(swaddr_t)ze)) + cur_lo;
+	print_asm("jbe %x", cur_addr);
+	if (cpu.ZF == 1 || cpu.CF == 1) cpu.eip = cur_addr;
+	return 0;
 }
 
 
