949b6e9ce9b7d04a7bc6ab88eb941bce87210eb8
tracer-ics2016 <tracer@njuics.org>
2016-10-13 01:31:55 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  01:31:55 up  1:35,  1 user,  load average: 0.01, 0.02, 0.00 92eb35b5f8622c9134f60f7eed3499f7e15b3d13

--- nemu/src/cpu/exec/ctf/jcc-template.h
+++ nemu/src/cpu/exec/ctf/jcc-template.h
@@ -14,6 +14,7 @@ make_helper(concat(jcc_be_,SUFFIX)) {
 	DATA_TYPE cur_lo = (DATA_TYPE)eip + instr_fetch(eip - DATA_BYTE, DATA_BYTE);
 	DATA_TYPE ze = ~0x0;
 	swaddr_t cur_addr = (eip & (~(swaddr_t)ze)) + cur_lo;
+	print_asm("jbe %x", cur_addr);
 	if (cpu.ZF == 1 || cpu.CF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
 	return 1 + DATA_BYTE;
 }
