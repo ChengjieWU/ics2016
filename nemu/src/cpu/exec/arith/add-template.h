fc472bc2accd668377faf6444cbe401061272399
tracer-ics2016 <tracer@njuics.org>
2016-10-15 14:34:55 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  14:34:55 up  1:49,  1 user,  load average: 0.00, 0.00, 0.00 630f98b52e2ab3374444beb36540c524b02d459d

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -1,36 +1,35 @@
 #include "cpu/exec/template-start.h"
 
-#define instr sub
+#define instr add
 
 static void do_execute() {
-	DATA_TYPE minuend = op_dest->val;
-	DATA_TYPE subtrahend = op_src->val;	
-	DATA_TYPE difference = minuend - subtrahend;
-	OPERAND_W(op_dest, difference);
+	DATA_TYPE dest = op_dest->val;
+	DATA_TYPE src = op_src->val;
+	DATA_TYPE sum = dest + src;
+	OPERAND_W(op_dest, sum);
 
 	//ZF
-	cpu.ZF = !difference;
+	cpu.ZF = !sum;
 	//SF
-	if (MSB(difference)) cpu.SF = 1;
+	if (MSB(sum)) cpu.SF = 1;
 	else cpu.SF = 0;
 	//OF
-	DATA_TYPE neg_subtrahend = ~subtrahend;
-	if (MSB(minuend) == MSB(neg_subtrahend) && MSB(minuend) != MSB(difference)) cpu.OF = 1;
+	if (MSB(src) == MSB(dest) && MSB(dest) != MSB(sum)) cpu.OF = 1;
 	else cpu.OF = 0;
 	//CF
-	if (subtrahend > minuend) cpu.CF = 1;
+	if (sum < dest) cpu.CF = 1;
 	else cpu.CF = 0;
 	//PF
 	cpu.PF = 1;
-	DATA_TYPE dpf = difference;
+	DATA_TYPE dpf = sum;
 	int loop_i = 0;
 	for (; loop_i < 8; loop_i++) {
 		if ((dpf & 0x1) == 1) cpu.PF = ~cpu.PF;
 		dpf = dpf >> 1;
 	}
-	//IF, DF were not affected
-	print_asm_template2();
+	//IF, DF were not affectd
 }
+
 
 make_instr_helper(i2a)
 make_instr_helper(i2rm)
