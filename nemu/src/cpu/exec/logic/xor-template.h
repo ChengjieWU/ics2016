b3d5a9a85fc39b2a9ee949a973e1aa552aaad1b2
tracer-ics2016 <tracer@njuics.org>
2016-10-16 05:56:27 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  05:56:27 up  5:51,  1 user,  load average: 0.02, 0.01, 0.00 ea69743f4dbc5b374c9e4dcbcf1017878fc29c5

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -6,8 +6,23 @@ static void do_execute () {
 	DATA_TYPE result = op_dest->val ^ op_src->val;
 	OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	//ZF
+	cpu.ZF = !result;
+	//SF
+	cpu.SF = MSB(result);
+	//PF
+	cpu.PF = 1;
+	DATA_TYPE dpf = result;
+	int loop_i = 0;
+	for (; loop_i < 8; loop_i++) {
+		if ((dpf & 0x1) == 1) cpu.PF = ~cpu.PF;
+		dpf = dpf >> 1;
+	}
+	//CF
+	cpu.CF = 0;
+	//OF
+	cpu.OF = 0;
+	//IF, DF were not affected
 
 	print_asm_template2();
 }
