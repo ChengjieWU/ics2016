fc1a67238d406e5536c8037cf7375729363b763b
tracer-ics2016 <tracer@njuics.org>
2016-10-16 05:49:54 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  05:49:54 up  5:44,  1 user,  load average: 0.00, 0.00, 0.00 65cc92a550965139afdbb9d4a7782233f3ea2421

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,43 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute() {
+	DATA_TYPE dest = op_dest->val;
+	DATA_TYPE src = op_src->val;
+	DATA_TYPE sum = dest + src + cpu.CF;
+	OPERAND_W(op_dest, sum);
+
+	//ZF
+	cpu.ZF = !sum;
+	//SF
+	cpu.SF = MSB(sum);
+	//OF
+	if (MSB(src) == MSB(dest) && MSB(dest) != MSB(sum)) cpu.OF = 1; //the condition remains the same
+	else cpu.OF = 0;
+	//CF
+	if (dest + src >= dest && sum >= cpu.CF) cpu.CF = 0;
+	else cpu.CF = 1;
+	//PF
+	cpu.PF = 1;
+	DATA_TYPE dpf = sum;
+	int loop_i = 0;
+	for (; loop_i < 8; loop_i++) {
+		if ((dpf & 0x1) == 1) cpu.PF = ~cpu.PF;
+		dpf = dpf >> 1;
+	}
+	//IF, DF were not affectd
+	
+	print_asm_template2();
+}
+
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
