f1cfd9824ac5621f2b13cf2848fc2e55648cf37f
tracer-ics2016 <tracer@njuics.org>
2016-10-17 04:50:22 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  04:50:22 up 17:35,  1 user,  load average: 0.00, 0.00, 0.00 23768ab1801c8434216bae2dd4e54d3c867aae0b

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -12,15 +12,15 @@ static void do_execute() {
 	cpu.SF = MSB(result_cmp);
 	//OF
 	DATA_TYPE m1 = 1 << (8 * DATA_BYTE - 1);
-	printf("%x %x %x\n", oprand_2, oprand_1, result_cmp);
+			//printf("%x %x %x\n", oprand_2, oprand_1, result_cmp);
 	if (oprand_1 == m1) {
 		if (oprand_2 != 0 && MSB(oprand_2) == 0) cpu.OF = 0;
-		else cpu.CF = 1;
+		else cpu.OF = 1;
 	}
 	else {
 		DATA_TYPE neg_op1 = (~oprand_1) + 1;
-		printf("%x\n", neg_op1);
-		printf("%d %d\n", MSB(neg_op1)==MSB(oprand_2), MSB(oprand_2)!=MSB(result_cmp));
+			//printf("%x\n", neg_op1);
+			//printf("%d %d\n", MSB(neg_op1)==MSB(oprand_2), MSB(oprand_2)!=MSB(result_cmp));
 		if (MSB(neg_op1) == MSB(oprand_2) && MSB(oprand_2) != MSB(result_cmp)) cpu.OF = 1;
 		else cpu.OF = 0;
 	}
