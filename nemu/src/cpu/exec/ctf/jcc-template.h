bd3de20e8fa852fa90dda5889775d2c8bcd639d6
tracer-ics2016 <tracer@njuics.org>
2016-10-18 16:38:30 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  16:38:30 up  1:03,  1 user,  load average: 0.00, 0.00, 0.00 2bbf44c790a4a5d5adc9960ed1b2a1b92768c2cc

--- nemu/src/cpu/exec/ctf/jcc-template.h
+++ nemu/src/cpu/exec/ctf/jcc-template.h
@@ -2,6 +2,7 @@
 
 #define instr jcc
 
+
 #if DATA_BYTE == 1 || DATA_BYTE == 4
 swaddr_t concat(next_addr_,SUFFIX)(swaddr_t eip) {
 	concat(decode_si_, SUFFIX)(eip + 1);
@@ -18,49 +19,49 @@ make_helper(concat(jcc_e_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("je %x", cur_addr);
 	if (cpu.ZF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_a_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("ja %x", cur_addr);
 	if (cpu.CF == 0 && cpu.ZF == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_ae_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jae %x", cur_addr);
 	if (cpu.CF == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_b_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jb %x", cur_addr);
 	if (cpu.CF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_be_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jbe %x", cur_addr);
 	if (cpu.ZF == 1 || cpu.CF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_ecxz_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jecxz %x", cur_addr);
 	if (cpu.gpr[1]._32 == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_g_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jg %x", cur_addr);
 	if (cpu.ZF == 0 && cpu.SF == cpu.OF) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_ge_,SUFFIX)) {
@@ -74,64 +75,63 @@ make_helper(concat(jcc_l_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jl %x", cur_addr);
 	if (cpu.SF != cpu.OF) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	printf("%x %x %x %x\n", cur_addr, cpu.eip, 1 + DATA_BYTE, cpu.eip + 1 + DATA_BYTE);
-	return  1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_le_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jle %x", cur_addr);
 	if (cpu.ZF == 1 || cpu.SF != cpu.OF) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_ne_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jne %x", cur_addr);
 	if (cpu.ZF == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_no_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jno %x", cur_addr);
 	if (cpu.OF == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_np_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jnp %x", cur_addr);
 	if (cpu.PF == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_ns_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jns %x", cur_addr);
 	if (cpu.SF == 0) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_o_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jo %x", cur_addr);
 	if (cpu.OF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_p_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("jp %x", cur_addr);
 	if (cpu.PF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 make_helper(concat(jcc_s_,SUFFIX)) {
 	swaddr_t cur_addr = concat(next_addr_,SUFFIX)(eip);
 	print_asm("js %x", cur_addr);
 	if (cpu.SF == 1) cpu.eip = cur_addr - (1 + DATA_BYTE);
-	return 1 + DATA_BYTE;	
+	return DATA_BYTE + (DATA_BYTE == 1);	
 }
 
 #include "cpu/exec/template-end.h"
