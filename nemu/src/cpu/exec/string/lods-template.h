1726cadba54c70299ce54db05a2d7fc49ac73d04
tracer-ics2016 <tracer@njuics.org>
2016-10-19 15:01:17 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:01:17 up  5:37,  1 user,  load average: 0.00, 0.00, 0.00 25ea4a34c3b4c1c8137cba7e58eedad72b1b4e70

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -7,13 +7,13 @@ make_helper(concat(lods_, SUFFIX)) {
 	if (cpu.DF == 0) cpu.esi += DATA_BYTE;
 	else cpu.esi -= DATA_BYTE;
 #if DATA_BYTE == 1
-	print_asm("lodsb %%ds:(%%esi),AL");
+	print_asm("lodsb %%ds:(%%esi),%%al");
 #endif
 #if	DATA_BYTE == 2
-	print_asm("lodsw %%ds:(%%esi),AX");
+	print_asm("lodsw %%ds:(%%esi),%%ax");
 #endif
 #if DATA_BYTE == 4
-	print_asm("lodsl %%ds:(%%esi),EAX");
+	print_asm("lodsl %%ds:(%%esi),%%eax");
 #endif
 	return 1;
 }
---
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -0,0 +1,50 @@
+#include "cpu/exec/template-start.h"
+
+#define instr scas
+
+make_helper(concat(scas_, SUFFIX)) {
+	DATA_TYPE oprand_1 = MEM_R(cpu.edi);
+	DATA_TYPE oprand_2 = REG(R_EAX);
+	DATA_TYPE result_cmp = oprand_2 - oprand_1;
+	//ZF
+	cpu.ZF = !result_cmp;
+	//SF
+	cpu.SF = MSB(result_cmp);
+	//OF
+	DATA_TYPE m1 = 1 << (8 * DATA_BYTE - 1);
+	if (oprand_1 == m1) {
+		cpu.OF = !MSB(oprand_2);
+	}
+	else {
+		DATA_TYPE neg_op1 = (~oprand_1) + 1;
+		if (MSB(neg_op1) == MSB(oprand_2) && MSB(oprand_2) != MSB(result_cmp)) cpu.OF = 1;
+		else cpu.OF = 0;
+	}
+	//CF
+	if (oprand_1 > oprand_2) cpu.CF = 1;
+	else cpu.CF = 0;
+	//PF
+	cpu.PF = 1;
+	DATA_TYPE dpf = result_cmp;
+	int loop_i = 0;
+	for (; loop_i < 8; loop_i++) {
+		if ((dpf & 0x1) == 1) cpu.PF = ~cpu.PF;
+		dpf = dpf >> 1;
+	}
+	//IF, DF were not affected
+
+	if (cpu.DF == 0) cpu.edi += DATA_BYTE;
+	else cpu.edi -= DATA_BYTE;
+#if DATA_BYTE == 1
+	print_asm("scasb %%es:(%%edi),%%al");
+#endif
+#if	DATA_BYTE == 2
+	print_asm("scasw %%es:(%%edi),%%ax");
+#endif
+#if DATA_BYTE == 4
+	print_asm("scasl %%es:(%%edi),%%eax");
+#endif
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
