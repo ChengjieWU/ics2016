33335bd438ae35639ed781934f6373295e06d7ee
tracer-ics2016 <tracer@njuics.org>
2016-10-19 15:11:15 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:11:15 up  5:47,  1 user,  load average: 0.01, 0.00, 0.00 842cdad1dbca21669fbce39560527826beb64aa4

---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,21 @@
+#include "cpu/exec/template-start.h"
+
+#define instr stos
+
+make_helper(concat(stos_, SUFFIX)) {
+	MEM_W(cpu.edi, REG(R_EAX));
+	if (cpu.DF == 0) cpu.edi += DATA_BYTE;
+	else cpu.edi -= DATA_BYTE;
+#if DATA_BYTE == 1
+	print_asm("stosb %%al,%%es:(%%edi)");
+#endif
+#if	DATA_BYTE == 2
+	print_asm("stosw %%ax,%%es:(%%edi)");
+#endif
+#if DATA_BYTE == 4
+	print_asm("stosl %%eax,%%es:(%%edi)");
+#endif
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
