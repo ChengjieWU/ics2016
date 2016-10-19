0bbd37325347b2180b70ce49a5d04da2187605ea
tracer-ics2016 <tracer@njuics.org>
2016-10-19 15:29:08 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:29:08 up  6:05,  1 user,  load average: 0.00, 0.00, 0.00 e6b145db12c146b89d8210f89d2cdfdee5062dff

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -74,12 +74,12 @@ make_group(group3_v,
 
 /* 0xfe */
 make_group(group4,
-	inv, dec_rm_b, inv, inv, 
+	inc_rm_b, dec_rm_b, inv, inv, 
 	inv, inv, inv, inv)
 
 /* 0xff */
 make_group(group5,
-	inv, dec_rm_v, inv, inv, 
+	inc_rm_v, dec_rm_v, inv, inv, 
 	jmp_rm_v, inv, push_m_v, inv)
 
 make_group(group6,
@@ -110,8 +110,8 @@ helper_fun opcode_table [256] = {
 /* 0x34 */	xor_i2a_b, xor_i2a_v, inv, inv,
 /* 0x38 */	cmp_r_rm_b, cmp_r_rm_v, cmp_rm_r_b, cmp_rm_r_v,
 /* 0x3c */	cmp_i_a_b, cmp_i_a_v, inv, inv,
-/* 0x40 */	inv, inv, inv, inv,
-/* 0x44 */	inv, inv, inv, inv,
+/* 0x40 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
+/* 0x44 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
 /* 0x48 */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
 /* 0x4c */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
 /* 0x50 */	push_r_v, push_r_v, push_r_v, push_r_v,
