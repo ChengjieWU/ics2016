19c825e89c358b92c20af858605fd7a58c369ef6
tracer-ics2016 <tracer@njuics.org>
2016-10-19 15:14:29 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:14:29 up  5:50,  1 user,  load average: 0.00, 0.00, 0.00 3e7f812a29566420229eb9c25fd66bad14d18817

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -28,7 +28,7 @@ make_helper(rep) {
 			/* TODO: Jump out of the while loop if necessary. */
 			if (ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae
 							|| ops_decoded.opcode == 0xaf) {
-				if (cpu.ZF == 1) break;
+				if (cpu.ZF == 0) break;
 			}
 		}
 		len = 1;
@@ -54,7 +54,7 @@ make_helper(repnz) {
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
 			  );
-		if (cpu.ZF == 0) break;
+		if (cpu.ZF == 1) break;
 
 		/* TODO: Jump out of the while loop if necessary. */
 
