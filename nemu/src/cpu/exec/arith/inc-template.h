9effe52a988640578c6d1043ce847284b761d9b5
tracer-ics2016 <tracer@njuics.org>
2016-10-19 15:32:31 +0000

    >  compile NEMU 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:32:31 up  6:08,  1 user,  load average: 0.00, 0.00, 0.00 92e1d1aa4f47a79939d05707dc1f61538d77fe5f

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -6,9 +6,27 @@ static void do_execute () {
 	DATA_TYPE result = op_src->val + 1;
 	OPERAND_W(op_src, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	//ZF
+	cpu.ZF = !result;
+	//SF
+	cpu.SF = MSB(result);
+	//OF
+	DATA_TYPE help = 0;
+	help = ((~help) << 1) >> 1;
+	cpu.OF = (op_src->val == help);
+	//CF
+	help = 0;
+	help = ~help;
+	cpu.CF = (op_src->val == help);
+	//PF
+	cpu.PF = 1;
+	DATA_TYPE dpf = result;
+	int loop_i = 0;
+	for (; loop_i < 8; loop_i++) {
+		if ((dpf & 0x1) == 1) cpu.PF = ~cpu.PF;
+		dpf = dpf >> 1;
+	}
+	//IF, DF were not affected
 	print_asm_template1();
 }
 
