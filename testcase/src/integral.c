9830f45e7af078afb4790b8ddda11022daa03af2
tracer-ics2016 <tracer@njuics.org>
2016-10-21 08:48:37 +0000

    >  run 151220122 wuchengjie Linux 9eea6112849f 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  08:48:37 up 22:03,  1 user,  load average: 0.00, 0.00, 0.00 948f6235d4e8e2043834e81d739591fc35b9c240

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -38,9 +38,8 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 		
-		switch(k){
-				case 1: {
-								/*FLOAT x = a + F_mul_int(h, k);
+		/*if (k == 1) {
+								FLOAT x = a + F_mul_int(h, k);
 								nemu_assert(Fabs(F_mul_F(x, x) - f2F(0.64)) < f2F(1e-4));
 								x = F_mul_int(F_mul_F(x, x), 25);
 								nemu_assert(Fabs(x - f2F(16)) < f2F(1e-4));
@@ -48,11 +47,11 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 								nemu_assert(Fabs(x - f2F(17)) < f2F(1e-4));
 								x = F_div_F(int2F(1), x);
 								nemu_assert(Fabs(x - f2F(0.058824)) < f2F(1e-4));
-								*/
+								
 								//F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
 								//nemu_assert(Fabs(s - f2F(0.097286)) < f2F(1e-4));	
-						}
-				case 2:{
+						}*/
+		if (k == 2) {
 							    //nemu_assert(h == f2F(0.2));
 								nemu_assert(k == 2);
 								//nemu_assert(F_mul_int(h, k) == f2F(0.4));	
@@ -63,7 +62,6 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 								//nemu_assert(Fabs(x - f2F(9)) < f2F(1e-4));
 								//nemu_assert(Fabs(s - f2F(0.197286)) < f2F(1e-4));	
 					   }
-		}
 		
 	}
 	s = F_mul_F(s, h);
