5a0c3a47a20dc8ba22128eaa69e6930672237718
tracer-ics2016 <tracer@njuics.org>
2016-11-04 10:20:39 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  10:20:39 up  2:54,  1 user,  load average: 0.15, 0.03, 0.01 527d32a15899cc723fc459af75f049e76bc130f3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -51,7 +51,7 @@ uint32_t loader() {
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void*)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz);
+			memset((void*)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
