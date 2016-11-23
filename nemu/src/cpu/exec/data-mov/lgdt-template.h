5a58d87e4187481286ca66bf6587d1b5e6b30c19
tracer-ics2016 <tracer@njuics.org>
2016-11-23 10:30:01 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  10:30:01 up 1 day,  7:57,  2 users,  load average: 0.00, 0.00, 0.00 64df2887389556c15f49263081781f87ad482955

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -12,7 +12,7 @@ make_helper(concat(lgdt_, SUFFIX)) {
 	int ret;
 	if (DATA_BYTE == 2) ret = 6;
 	else ret = 7;
-	print_asm("lgdt %hu%u", t1, t2);
+	print_asm("lgdt 0x%hx%x", t1, t2);
 	return ret;
 }
 
