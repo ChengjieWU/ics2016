0859a9ad66821f00f710c44a0a619034b089bac8
tracer-ics2016 <tracer@njuics.org>
2016-11-23 10:38:51 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  10:38:51 up 1 day,  8:05,  2 users,  load average: 0.05, 0.02, 0.00 f4d5c6dfe27b6e008afc519cbebba2ca42c13561

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -5,15 +5,17 @@
 make_helper(concat(lgdt_, SUFFIX)) {
 	uint16_t t1 = instr_fetch(eip + 1, 2);
 	uint32_t t2;
-	if (DATA_BYTE == 2) t2 = instr_fetch(eip + 3, 3) & 0x00111111;
-	else t2 = instr_fetch(eip + 3, 4);
+	//if (DATA_BYTE == 2) t2 = instr_fetch(eip + 3, 3) & 0x00111111;
+	//else t2 = instr_fetch(eip + 3, 4);
+	t2 = instr_fetch(eip + 3, 3) & 0x00111111;
 	cpu.gdtr.limit = t1;
 	cpu.gdtr.base = t2;
-	int ret;
-	if (DATA_BYTE == 2) ret = 6;
-	else ret = 7;
+	//int ret;
+	//if (DATA_BYTE == 2) ret = 6;
+	//else ret = 7;
 	print_asm("lgdt 0x%hx%x", t1, t2);
-	return ret;
+	//return ret;
+	return 6;
 }
 
 
