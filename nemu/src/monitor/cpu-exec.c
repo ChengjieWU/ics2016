af4bebe350ef5f86a23a502655d9f82ade0c91de
tracer-ics2016 <tracer@njuics.org>
2016-11-23 15:18:00 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  15:18:00 up 1 day, 12:19,  2 users,  load average: 0.01, 0.01, 0.00 b9ebbd954582d52cb124b4246d063c5a4728cce

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -63,7 +63,7 @@ void cpu_exec(volatile uint32_t n) {
 		 * instruction decode, and the actual execution. */
 		int instr_len = exec(cpu.eip);
 		cpu.eip += instr_len;
-		printf("finish\n");
+		printf("about to finish\n");
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
@@ -72,7 +72,7 @@ void cpu_exec(volatile uint32_t n) {
 			printf("%s\n", asm_buf);
 		}
 #endif
-
+		printf("finish\n");
 		/* TODO: check watchpoints here. */
 		int stop = check_wp();
 		if (stop != 0) {
