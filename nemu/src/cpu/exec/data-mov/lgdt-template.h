ef7d9afa3fdb7bb7368a0c6a31a7b4d18a6456c1
tracer-ics2016 <tracer@njuics.org>
2016-11-24 01:50:48 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  01:50:48 up 1 day, 15:19,  2 users,  load average: 0.00, 0.00, 0.00 6f97a0e24324cd1a8cbdafbf1e51ad056919bfbe

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #define instr lgdt
 
-make_helper(concat(lgdt_, SUFFIX)) {
+/*make_helper(concat(lgdt_, SUFFIX)) {
 	int len = decode_rm2r_l(eip + 1);
 	printf("%x\n", op_src->val);
 	cpu.gdtr.limit = lnaddr_read(op_src->val, 2);
@@ -10,8 +10,16 @@ make_helper(concat(lgdt_, SUFFIX)) {
 	else cpu.gdtr.base = lnaddr_read(op_src->val + 2, 4);
 	print_asm("lgdt 0x%hx 0x%x", cpu.gdtr.limit, cpu.gdtr.base);
 	return len + 1;
-}
+}*/
 
+
+make_helper(concat(lgdt_, SUFFIX)) {
+	op_src->val = instr_fetch(eip + 2, 4);
+	if (DATA_BYTE == 2) cpu.gdtr.base = lnaddr_read(op_src->val + 2, 3);
+	else cpu.gdtr.base = lnaddr_read(op_src->val + 2, 4);
+	print_asm("lgdt 0x%hx 0x%x", cpu.gdtr.limit, cpu.gdtr.base);
+	return 6;
+}
 
 
 
