0b8a33b1fbd7b3fd6fae0cbdc6c6993c7bc78f00
tracer-ics2016 <tracer@njuics.org>
2016-11-24 02:30:42 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  02:30:42 up 1 day, 15:59,  2 users,  load average: 0.03, 0.01, 0.00 e5225e1ef677f31223f398425775c9f83677b26e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -33,6 +33,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 }
 
 lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg) {
+	assert(sreg == 0 || sreg == 1 || sreg == 2 || sreg == 3);
 	lnaddr_t ret_addr = addr;
 	if (cpu_cr0_protect_enable()) {
 		ret_addr += cpu_sreg_cache_base(sreg);
