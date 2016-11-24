d4b448bbc46d2bc05cc8bba54adb8059e4e3e8ba
tracer-ics2016 <tracer@njuics.org>
2016-11-24 03:31:57 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  03:31:57 up 1 day, 17:00,  2 users,  load average: 0.14, 0.03, 0.01 3d3c2d89b1132040c4e9ce15d83ec45690673c1

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -113,7 +113,7 @@ int read_ModR_M(swaddr_t eip, Operand *rm, Operand *reg) {
 	}
 	else {
 		int instr_len = load_addr(eip, &m, rm);
-		rm->val = swaddr_read(rm->addr, rm->size, m.reg);
+		rm->val = swaddr_read(rm->addr, rm->size, rm->sreg);
 		return instr_len;
 	}
 }
