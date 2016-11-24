531d2a05d070b022cb5911e52b15e7c9509b2c96
tracer-ics2016 <tracer@njuics.org>
2016-11-24 05:16:54 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  05:16:54 up 1 day, 17:33,  2 users,  load average: 0.00, 0.00, 0.00 e8614d90dbd00a68a704d028c147a9a4f83dda0e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -99,7 +99,7 @@ static int cmd_info(char *args) {
 					   	cpu.OF, cpu.DF, cpu.IF, cpu.SF, cpu.ZF, cpu.PF, cpu.CF);
 		printf("es\t%hx\t\tcs\t%hx\nss\t%hx\t\tds\t%hx\n", cpu.sreg[0]._16, cpu.sreg[1]._16, cpu.sreg[2]._16, cpu.sreg[3]._16);
 		printf("cs_cache %hx%hx%hx\n", cpu.sreg[1].cache.base_31_24, cpu.sreg[1].cache.base_23_16, cpu.sreg[1].cache.base_15_0);
-		printf("es_cache %hx\n", cpu.sreg[0].cache.base_15_0);
+		printf("es_cache %hx%hx%hx\n", cpu.sreg[0].cache.base_31_24, cpu.sreg[0].cache.base_23_16, cpu.sreg[0].cache.base_15_0);
 		printf("ss_cache %hx\n", cpu.sreg[2].cache.base_15_0);
 		printf("ds_cache %hx\n", cpu.sreg[3].cache.base_15_0);
 		printf("gdtr\t%hx\t%x\n", cpu.gdtr.limit, cpu.gdtr.base);
