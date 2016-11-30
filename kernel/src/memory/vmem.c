6635c29dda2cc942f5a387c05286de5e23f4fe75
tracer-ics2016 <tracer@njuics.org>
2016-11-30 13:31:44 +0000

    >  compile kernel 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  13:31:44 up 11:17,  1 user,  load average: 1.00, 1.00, 0.96 931c27f836e5b64ca317f61d4fc30493963bc67

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -5,6 +5,9 @@
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
 
+static PTE uptable[NR_PTE];
+
+
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
 
@@ -14,7 +17,15 @@ void create_video_mapping() {
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	//panic("please implement me");
+	memset(uptable, 0, NR_PTE * sizeof(PTE));
+	PDE* updir = get_updir();
+	updir[(VMEM_ADDR >> 22) & 0x3ff].val = make_pde(uptable);
+	int start = (VMEM_ADDR >> 12) & 0x3ff;
+	int i = 0;
+	for (; i <= SCR_SIZE / PAGE_SIZE + 1; i++) {
+		uptable[start + i].val = VMEM_ADDR + PAGE_SIZE * i;
+	}
 }
 
 void video_mapping_write_test() {
