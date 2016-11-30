02f82812f6a03f91b7d42cfe4c1f94f41aef614a
tracer-ics2016 <tracer@njuics.org>
2016-11-30 03:26:43 +0000

    >  compile kernel 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  03:26:43 up  3:59,  1 user,  load average: 0.19, 0.06, 0.02 962775eb8f0d132489cc44433d258bf19e68b6d1

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -45,14 +45,14 @@ uint32_t loader() {
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
 			//uint8_t buff[4096];
-			uint32_t addr = mm_malloc((void*)ph->p_vaddr, ph->p_memsz);
-			ramdisk_read(addr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+			uint32_t addr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			ramdisk_read((void*)addr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
 			//memcpy((void*)ph->p_vaddr, buff, ph->p_filesz);
 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset(addr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memset((void*)addr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
