f2d045cbb482b262e1fe35b35684bba3e6d2992f
tracer-ics2016 <tracer@njuics.org>
2016-12-14 02:36:52 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  02:36:52 up 19:25,  1 user,  load average: 0.00, 0.00, 0.00 bc8d0568ec47f71e62fa85297250c5b425a8aa6d

--- nemu/src/cpu/exec/intr.c
+++ nemu/src/cpu/exec/intr.c
@@ -16,7 +16,21 @@ void raise_intr(uint8_t NO) {
 	swaddr_write(cpu.esp, 2, cpu.sreg[1]._16, 2);
 	cpu.esp -= 4;
 	swaddr_write(cpu.esp, 4, cpu.eip, 2);
-	
+
+	uint16_t idt_selector = lnaddr_read(cpu.idtr.base + 8 * NO + 2, 2);
+	cpu.sreg[1]._16 = idt_selector;
+	cpu.sreg[1].cache.base_15_0 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 2, 2);
+	cpu.sreg[1].cache.base_23_16 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 4, 1);
+	cpu.sreg[1].cache.base_31_24 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 7, 1);
+	cpu.sreg[1].cache.limit_15_0 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX, 2);
+	cpu.sreg[1].cache.limit_19_16 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 6, 1) & 0xf;
+
+	uint32_t base_addr = (cpu.sreg[1].cache.base_31_24 << 24) | (cpu.sreg[1].cache.base_23_16 << 16) | cpu.sreg[1].cache.base_15_0;
+	uint16_t offset1 = lnaddr_read(cpu.idtr.base + 8 * NO, 2);
+	uint16_t offset2 = lnaddr_read(cpu.idtr.base + 8 * NO + 6, 2);
+	uint32_t offset_total = (offset2 << 16) | offset1;
+
+	cpu.eip = base_addr + offset_total;
 
 	longjmp(jbuf, 1);
 }
