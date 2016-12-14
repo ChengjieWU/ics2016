3272222c76f0e4d92892a30f147b9e82f829eae3
tracer-ics2016 <tracer@njuics.org>
2016-12-14 01:59:18 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  01:59:18 up 18:47,  1 user,  load average: 0.00, 0.01, 0.00 34ffa7811b6c612f21192b206eb9cc2d331a617c

--- nemu/src/cpu/exec/intr.c
+++ nemu/src/cpu/exec/intr.c
@@ -1,4 +1,7 @@
 #include "nemu.h"
+
+void swaddr_write(swaddr_t, size_t, uint32_t, uint8_t);
+
 #include <setjmp.h>
 extern jmp_buf jbuf;
 void raise_intr(uint8_t NO) {
@@ -7,7 +10,12 @@ void raise_intr(uint8_t NO) {
 	 *       */
 	/* Jump back to cpu_exec() */
 
-	cpu.eip = 1;
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eflags, 2);
+	//cpu.esp -= 2;
+	//swaddr_write(cpu.esp, 2, cpu.sreg[1], 2);
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eip, 2);
 
 	longjmp(jbuf, 1);
 }
