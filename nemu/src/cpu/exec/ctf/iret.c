74dc43cdbbedf6332d6ec6cd91e9f20c7e001e6a
tracer-ics2016 <tracer@njuics.org>
2016-12-14 02:59:27 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  02:59:27 up 19:47,  1 user,  load average: 0.00, 0.00, 0.00 6a342f9ba57bc327216335a5d8cc86f79e257cc2

---
+++ nemu/src/cpu/exec/ctf/iret.c
@@ -0,0 +1,15 @@
+#include "cpu/exec/helper.h"
+
+uint32_t swaddr_read(swaddr_t, size_t, uint8_t);
+
+make_helper(iret) {
+	//if (cpu.PE == 0) {
+		cpu.eip = swaddr_read(cpu.esp, 4, 2);
+		cpu.esp += 4;
+		cpu.sreg[1]._16 = swaddr_read(cpu.esp, 2, 2);
+		cpu.esp += 2;
+		cpu.eflags = swaddr_read(cpu.esp, 4, 2);
+		cpu.esp += 4;
+		return 0;
+	//}
+}
