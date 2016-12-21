fc27f6d073f19ecfdb48395ea9c0c5d3fdec17ce
tracer-ics2016 <tracer@njuics.org>
2016-12-21 14:03:02 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  14:03:02 up 10:45,  3 users,  load average: 0.00, 0.01, 0.00 35c9deadd2d203d313592923495ba02d84eb270b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -111,6 +111,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 	}
 	else {
 		hwaddr_t hwaddr = page_translate(addr);
+		if (addr == 0xc015d47e) printf("%x, %d\n", hwaddr, len);	
 		hwaddr_write(hwaddr, len, data);
 	}
 }
@@ -149,6 +150,6 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data, uint8_t sreg) {
 	assert(len == 1 || len == 2 || len == 4);
 	lnaddr_t lnaddr = seg_translate(addr, len, sreg);
-	if (addr == 0xc015d47e) printf("%x\n", lnaddr);	
+	if (addr == 0xc015d47e) printf("%x, %d\n", lnaddr, len);	
 	return lnaddr_write(lnaddr, len, data);
 }
