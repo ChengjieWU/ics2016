82d51677bd80e5abf21d43bd6d2313552c6f70ca
tracer-ics2016 <tracer@njuics.org>
2016-12-21 13:59:40 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  13:59:40 up 10:41,  3 users,  load average: 0.00, 0.00, 0.00 2424217950313484932553c5334dd489c930b91f

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -14,15 +14,15 @@ make_helper(concat(movs_, SUFFIX)) {
 		cpu.esi -= 1;
 	}
 #else
-	int x = DATA_BYTE;
-	uint32_t temp = MEM_R(REG(R_ESI), 3);
+	//int x = DATA_BYTE;
+	//uint32_t temp = MEM_R(REG(R_ESI), 3);
 	//MEM_W(REG(R_EDI), temp, 0);
-	swaddr_write(REG(R_EDI), x, temp, 0);
-	//MEM_W(REG(R_EDI), MEM_R(REG(R_ESI), 3), 0);
-	if (REG(R_EDI) == 0xc015d47e)
-	{
-		printf("%d, %x, %x, %x\n", x, REG(R_ESI), temp, MEM_R(REG(R_EDI), 3));
-	}
+	//swaddr_write(REG(R_EDI), x, temp, 0);
+	MEM_W(REG(R_EDI), MEM_R(REG(R_ESI), 3), 0);
+	//if (REG(R_EDI) == 0xc015d47e)
+	//{
+		//printf("%d, %x, %x, %x\n", x, REG(R_ESI), temp, MEM_R(REG(R_EDI), 3));
+	//}
 	if (cpu.DF == 0) {
 		REG(R_ESI) += DATA_BYTE;
 		REG(R_EDI) += DATA_BYTE;
