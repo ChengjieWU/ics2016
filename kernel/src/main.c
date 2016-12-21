ff4eafd7b7c741e5d0efe0254d06dd1b0db8792f
tracer-ics2016 <tracer@njuics.org>
2016-12-21 10:30:54 +0000

    >  compile kernel 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  10:30:54 up  7:12,  1 user,  load average: 0.00, 0.00, 0.00 c7c3b3ea15b827dcc11b16cc40d10f05723a96c3

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -74,7 +74,7 @@ void init_cond() {
 	 * the serial port is available in NEMU.
 	 */
 	
-	Log("Hello, NEMU world!");
+	Log("Hello, Hello, Hello, Hello, NEMU world!");
 
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
