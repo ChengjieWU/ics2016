f4e95b61749be3d62cb00e809b5c125717306323
tracer-ics2016 <tracer@njuics.org>
2016-12-21 09:16:55 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  09:16:55 up  7:07,  1 user,  load average: 0.00, 0.00, 0.00 ac23c01b40c4fd2f9198dc77640aa2a3205fa340

--- nemu/src/cpu/exec/io/in-template.h
+++ nemu/src/cpu/exec/io/in-template.h
@@ -2,7 +2,7 @@
 
 #include "cpu/exec/template-start.h"
 #define instr in
-
+/*
 make_helper(concat(in_i_, SUFFIX)) {
 	print_asm("in");
 	uint16_t addr = instr_fetch(eip + 1, 1);
@@ -14,6 +14,20 @@ make_helper(concat(in_d_, SUFFIX)) {
 	print_asm("in");
 	uint16_t addr = cpu.gpr[2]._16;
 	REG(R_EAX) = pio_read(addr, DATA_BYTE);
+	return 1;
+}
+*/
+
+make_helper(concat(in_i_, SUFFIX)) {
+	uint32_t len = decode_i_b(eip + 1);
+	REG(R_EAX) = pio_read(op_src->val, DATA_BYTE);
+	print_asm("in");
+	return 1 + len;
+}
+
+make_helper(concat(in_d_, SUFFIX)) {
+	REG(R_EAX) = pio_read(reg_w(R_DX), DATA_BYTE);
+	print_asm("in");
 	return 1;
 }
 
--- nemu/src/cpu/exec/io/out-template.h
+++ nemu/src/cpu/exec/io/out-template.h
@@ -2,7 +2,7 @@
 
 #include "cpu/exec/template-start.h"
 #define instr out
-
+/*
 make_helper(concat(out_i_, SUFFIX)) {
 	print_asm("out");
 	uint16_t addr = instr_fetch(eip + 1, 1);
@@ -16,5 +16,18 @@ make_helper(concat(out_d_, SUFFIX)) {
 	pio_write(addr, DATA_BYTE, (uint32_t)REG(R_EAX));
 	return 1;
 }
+*/
+make_helper(concat(out_i_, SUFFIX)) {
+	uint32_t len = decode_i_b(eip + 1);
+	pio_write(op_src->val, DATA_BYTE, REG(R_EAX));
+	return 1 + len;
+}
+
+make_helper(concat(out_d_, SUFFIX)) {
+	pio_write(reg_w(R_DX), DATA_BYTE, REG(R_EAX));
+	return 1;
+}
+
+
 
 #include "cpu/exec/template-end.h"
