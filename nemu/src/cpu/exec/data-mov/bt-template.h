830141f1b6abe42ee0cae721774143e355cc9350
tracer-ics2016 <tracer@njuics.org>
2016-12-23 12:42:03 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  12:42:03 up  3:03,  3 users,  load average: 0.04, 0.02, 0.09 e4dcd253b32268138c22b7873f73e9ab86d5118

--- nemu/src/cpu/exec/data-mov/bt-template.h
+++ nemu/src/cpu/exec/data-mov/bt-template.h
@@ -10,13 +10,30 @@ make_helper(concat(bt_i2rm_, SUFFIX)) {
 	else cpu.CF = 0;
 	return 1 + len;
 }
-
+/*
 make_helper(concat(bt_r2rm_, SUFFIX)) {
 	int len = concat(decode_r2rm_, SUFFIX)(eip + 1);
 	uint32_t mask = 1;
 	mask = mask << (op_src->val % DATA_BYTE);
 	if (op_dest->val & mask) cpu.CF = 1;
 	else cpu.CF = 0;
+	return 1 + len;
+}
+*/
+
+make_helper(concat(bt_r2rm_, SUFFIX)) {
+	int len = concat(decode_r2rm_, SUFFIX)(eip + 1);
+	if (op_dest->type == OP_TYPE_REG)
+	{
+		uint32_t offset = op_src->val % (DATA_BYTE * 8);
+		cpu.CF = (op_dest->val >> offset) & 1;
+	}
+	else
+	{
+		uint32_t addr = op_src->val / (DATA_BYTE * 8) + op_dest->addr;
+		uint32_t offset = op_src->val % (DATA_BYTE * 8);
+		cpu.CF = (swaddr_read(addr, DATA_BYTE, op_dest->sreg) >> offset) & 1;
+	}
 	return 1 + len;
 }
 
