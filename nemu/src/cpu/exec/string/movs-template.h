2294d36dbce13d770cfdc948eb849a5207e28f73
tracer-ics2016 <tracer@njuics.org>
2016-12-23 07:26:18 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  07:26:18 up 10:15,  3 users,  load average: 0.04, 0.06, 0.04 cac40873858fdba22fdb0a9c865a54744890e02a

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,11 +3,11 @@
 #define instr movs
 
 make_helper(concat(movs_, SUFFIX)) {
-	if (DATA_BYTE == 1)
-	/*if (cpu.esi == 0xc0102f83)	once dubug
+	/*if (DATA_BYTE == 1)
+	if (cpu.esi == 0xc0102f83)	once dubug
 	{
 		printf("%x, %x", swaddr_read(cpu.esi, 4, 0), swaddr_read(cpu.edi, 4, 0));
-	}*/
+	}
 	//uint32_t temp = swaddr_read(cpu.esi, 1, 0);
 	//swaddr_write(cpu.edi, 1, temp, 0);
 	//MEM_W(cpu.edi, cpu.esi, 0);
@@ -30,7 +30,17 @@ make_helper(concat(movs_, SUFFIX)) {
 	else {
 		REG(R_ESI) -= DATA_BYTE;
 		REG(R_EDI) -= DATA_BYTE;
-	}}
+	}}*/
+	MEM_W(cpu.edi, MEM_R(cpu.esi, 3), 0);
+	if (cpu.DF == 0) {
+		REG(R_ESI) += DATA_BYTE;
+		REG(R_EDI) += DATA_BYTE;
+	}
+	else {
+		REG(R_ESI) -= DATA_BYTE;
+		REG(R_EDI) -= DATA_BYTE;
+	}
+	
 #if DATA_BYTE == 1
 	print_asm("movsb %%ds:(%%esi),%%es:(%%edi)");
 #endif
