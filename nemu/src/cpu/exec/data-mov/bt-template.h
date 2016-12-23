3e5a685b01eced97eb4c91dcb1725170dd27b8a0
tracer-ics2016 <tracer@njuics.org>
2016-12-23 13:46:46 +0000

    >  compile NEMU 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  13:46:46 up  4:07,  3 users,  load average: 0.02, 0.01, 0.00 c95ec0f6e5f379f3d79613d44462730995e336ea

--- nemu/src/cpu/exec/data-mov/bt-template.h
+++ nemu/src/cpu/exec/data-mov/bt-template.h
@@ -4,38 +4,37 @@
 make_helper(concat(bt_i2rm_, SUFFIX)) {
 	int len = concat(decode_rm_, SUFFIX)(eip + 1);
 	len += decode_i_b(eip + 1 + len);
-	uint32_t mask = 1;
-	mask = mask << (op_src->val % DATA_BYTE);
-	if (op_dest->val & mask) cpu.CF = 1;
-	else cpu.CF = 0;
-	return 1 + len;
-}
-
-make_helper(concat(bt_r2rm_, SUFFIX)) {
-	int len = concat(decode_r2rm_, SUFFIX)(eip + 1);
-	uint32_t mask = 1;
-	mask = mask << (op_src->val % (8 * DATA_BYTE));
-	if (op_dest->val & mask) cpu.CF = 1;
-	else cpu.CF = 0;
-	return 1 + len;
-}
-
-/*
-make_helper(concat(bt_r2rm_, SUFFIX)) {
-	int len = concat(decode_r2rm_, SUFFIX)(eip + 1);
 	if (op_dest->type == OP_TYPE_REG)
 	{
-		uint32_t offset = op_src->val % (DATA_BYTE * 8);
-		cpu.CF = (op_dest->val >> offset) & 1;
+		uint32_t mask = 1;
+		mask = mask << (op_src->val % (DATA_BYTE * 8));
+		cpu.CF = !(!(op_dest->val & mask));
 	}
 	else
 	{
 		uint32_t addr = op_src->val / (DATA_BYTE * 8) + op_dest->addr;
 		uint32_t offset = op_src->val % (DATA_BYTE * 8);
-		cpu.CF = (swaddr_read(addr, DATA_BYTE, op_dest->sreg) >> offset) & 1;
+		cpu.CF = (swaddr_read(addr, DATA_BYTE, op_dest->sreg) >> offset) & 0x1;
 	}
 	return 1 + len;
 }
-*/
+
+make_helper(concat(bt_r2rm_, SUFFIX)) {
+	int len = concat(decode_r2rm_, SUFFIX)(eip + 1);
+	if (op_dest->type == OP_TYPE_REG)
+	{
+		uint32_t mask = 1;
+		mask = mask << (op_src->val % (DATA_BYTE * 8));
+		cpu.CF = !(!(op_dest->val & mask));
+	}
+	else
+	{
+		//In this case, the second operand indicates the offset in memory, not in the value of the first operand.
+		uint32_t addr = op_src->val / (DATA_BYTE * 8) + op_dest->addr;
+		uint32_t offset = op_src->val % (DATA_BYTE * 8);
+		cpu.CF = (swaddr_read(addr, DATA_BYTE, op_dest->sreg) >> offset) & 0x1;
+	}
+	return 1 + len;
+}
 
 #include "cpu/exec/template-end.h"
