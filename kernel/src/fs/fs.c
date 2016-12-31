afc62b2b83aedc1d32fed3cbb6b15cd043b8a484
tracer-ics2016 <tracer@njuics.org>
2016-12-31 09:40:25 +0000

    >  compile game 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  09:40:25 up  1:43,  3 users,  load average: 0.08, 0.02, 0.01 8819e38fc4bce1b665a6cc68d037d98b193fd5c4

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -38,3 +38,68 @@ void ide_write(uint8_t *, uint32_t, uint32_t);
 
 /* TODO: implement a simplified file system here. */
 
+
+typedef struct {
+	bool opened;
+	uint32_t offset;
+} Fstate[NR_FILES + 3];
+
+int fs_open(const char *pathname, int flags) {
+	for (int i = 0; i < NR_FILES; i++) {
+		if (strcmp(file_table[i].name, pathname) == 0) {
+			int fd = i + 3;
+			f_state[fd].opened = true;
+			f_state[fd].offset = 0;
+			return fd;
+		}
+	}
+	panic("file not found");
+	return -1;
+}
+
+int fs_read(int fd, void *buf, size_t len) {
+	    if (fd < 3) return -1;
+		    assert(fd < NR_FILES + 3);
+			    assert(f_state[fd].opened);
+				    int max_len = file_table[fd - 3].size - f_state[fd].offset;
+					    assert(max_len >= 0);
+						    if (max_len < len) len = max_len;
+							    ide_read(buf, file_table[fd - 3].disk_offset + f_state[fd].offset, len);
+								    f_state[fd].offset += len;
+									    return len;
+}
+
+int fs_write(int fd, const void *buf, size_t len) {
+	    if (fd < 3) return -1;
+		    assert(fd < NR_FILES + 3);
+			    assert(f_state[fd].opened);
+				    int max_len = file_table[fd - 3].size - f_state[fd].offset;
+					    assert(max_len >= 0);
+						    if (max_len < len) len = max_len;
+							    ide_write(buf, file_table[fd - 3].disk_offset + f_state[fd].offset, len);
+								    f_state[fd].offset += len;
+									    return len;
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence) {
+	    if (fd < 3) return -1;
+		    assert(fd < NR_FILES + 3);
+			    assert(f_state[fd].opened);
+				    switch (whence) {
+						        case SEEK_SET: f_state[fd].offset = 0; break;
+											           case SEEK_CUR: break;
+																	          case SEEK_END: f_state[fd].offset = file_table[fd - 3].size; break;
+																							         default: assert(0);
+																											      }
+					    f_state[fd].offset += offset;
+						    assert(f_state[fd].offset >= 0);
+							    assert(f_state[fd].offset <= file_table[fd - 3].size);
+								    return f_state[fd].offset;
+}
+
+int fs_close(int fd) {
+	    assert(f_state[fd].opened);
+		    f_state[fd].opened = false;
+			    return 0;
+}
+
