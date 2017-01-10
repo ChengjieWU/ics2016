44239376e1bbc45a0b5d75d81de75eef94c1b3e0
tracer-ics2016 <tracer@njuics.org>
2017-01-10 10:34:50 +0000

    >  compile game 151220122 wuchengjie Linux c8d2c8bf978c 4.4.16-boot2docker #1 SMP Fri Jul 29 00:13:24 UTC 2016 x86_64 GNU/Linux  10:34:50 up  1:22,  3 users,  load average: 0.34, 0.79, 0.70 354af4ef28734ea13a25009a8ea585106fa6bcb2

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -30,11 +30,17 @@ keyboard_event(void) {
 	{
 		case KEY_STATE_EMPTY:
 			if (!(scancode & 0x80))
+			{
+				Log("press\n");
 				key_state[i] = KEY_STATE_PRESS;
+			}
 			break;
 		case KEY_STATE_WAIT_RELEASE:
 			if (scancode & 0x80)
+			{
+				Log("release\n");
 				key_state[i] = KEY_STATE_RELEASE;
+			}
 			break;
 		default:
 			break;
